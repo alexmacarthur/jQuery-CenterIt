20ef568d46db274b979d3a1ab469e68f268d0a17
alexmacarthur <alex@macarthur.me>
2015-04-23 23:26:30 -0500

    Enabled plugin to work dynamically when window resizes.

--- jquery.centerit.js
+++ jquery.centerit.js
@@ -3,7 +3,7 @@
       /**
        * Title: CenterIt
        * Author: Alex MacArthur
-       * Version: 1.0
+       * Version: 1.1
        * Description: A small plugin that centers an element within a parent element vertically and horizontally.
        */
 
@@ -19,18 +19,37 @@
               leftValue     = (parentWidth - thisWidth)/2,
               topValue      = (parentHeight - thisHeight)/2;
 
-          // apply relative positioning to parent so 'top' & 'left' values will have correct reference point
-          $parent.css({
-              'position'    : 'relative'
-          });
+          function setCSS(){
+            // apply relative positioning to parent so 'top' & 'left' values will have correct reference point
+            $parent.css({
+                'position'    : 'relative'
+            });
 
-          // apply css to element to center it relative to parent
-          $this.css({
-              'position'    : 'absolute',
-              'left'        : leftValue,
-              'top'         : topValue
-          });
+            // apply css to element to center it relative to parent
+            $this.css({
+                'position'    : 'absolute',
+                'left'        : leftValue,
+                'top'         : topValue
+            });
+          }
 
+          // set CSS when window first loads
+          setCSS();
+
+          // reapply CSS when window changes in size
+          $(window).resize(function(){
+
+            //reset dynamic variables when window resizes
+            thisHeight    = $this.height(),
+            thisWidth     = $this.outerWidth(),
+            parentHeight  = $parent.height(),
+            parentWidth   = $parent.outerWidth(),
+            leftValue     = (parentWidth - thisWidth)/2,
+            topValue      = (parentHeight - thisHeight)/2;
+
+            setCSS();
+
+          });
       };
         
 })(jQuery);
